e24af32e223e9e3ddda381f05edab58eead5f300
EMMAN <emmanuelbarrameda1@gmail.com>
2023-05-14 00:29:07 +0800

    051423 12:28am

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const text = "emmanpbarrameda;";
+const name1 = "emmanpbarrameda";
+const name2 = "Emmanuel P. Barrameda";
 let i = 0;
+let currentName = name1;
 
 function type() {
-  if (i < text.length) {
-    document.getElementById("typing-myname").innerHTML += text.charAt(i);
+  if (i < currentName.length) {
+    document.getElementById("typing-myname").innerHTML += currentName.charAt(i);
     i++;
     setTimeout(type, 100);
   } else {
     setTimeout(function() {
       i = 0;
       document.getElementById("typing-myname").innerHTML = "";
+      currentName = currentName === name1 ? name2 : name1;
       type();
     }, 2000);
   }
