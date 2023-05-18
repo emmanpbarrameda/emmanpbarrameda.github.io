32e3569b32f4905b25ca915ea375975a8df75216
EMMAN <emmanuelbarrameda1@gmail.com>
2023-05-19 01:26:06 +0800

    125am

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-const name1 = "Emmanuel P. Barrameda";
-const name2 = "emmanpbarrameda";
+const name1 = "I'm a Freelancer";
+const name2 = "I'm a Student";
+const name3 = "I'm a Desktop Developer";
+
 let i = 0;
 let currentName = name1;
 
 function type() {
   if (i < currentName.length) {
-    document.getElementById("typing-myname").innerHTML += currentName.charAt(i);
+    document.getElementById("profession").innerHTML += currentName.charAt(i);
     i++;
     setTimeout(type, 100);
   } else {
     setTimeout(function() {
       i = 0;
-      document.getElementById("typing-myname").innerHTML = "";
-      currentName = currentName === name1 ? name2 : name1;
+      document.getElementById("profession").innerHTML = "";
+      if (currentName === name1) {
+        currentName = name2;
+      } else if (currentName === name2) {
+        currentName = name3;
+      } else {
+        currentName = name1;
+      }
       type();
     }, 2000);
   }
 }
 
-
-
-
 window.onload = function() {
   type();
 };
-
-
