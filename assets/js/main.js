a55433a2b457a3d289d511f2a31d6ca17aa26836
EMMAN <emmanuelbarrameda1@gmail.com>
2023-06-20 03:30:37 +0800

    added animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,6 +38,10 @@ work();
 
 
 
+
+
+
+
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
