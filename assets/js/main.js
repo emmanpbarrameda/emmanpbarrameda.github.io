c70bce4b4c213959e3cce562d7cb0a74ec3038fe
EMMAN <emmanuelbarrameda1@gmail.com>
2023-12-16 02:19:00 +0800

    218AM 121623

--- assets/js/main.js
+++ assets/js/main.js
@@ -1010,3 +1010,15 @@ for (let i = 0; i < links.length; i++) {
     this.classList.remove('hovered-link');
   });
 }
+
+
+var url = 'https://emmanpbarrameda.github.io';
+
+var updateGoSquared = function() {
+  $.getJSON('https://api.gosquared.com/now/v3/pages?api_key=demo&site_token=GSN-106863-S&href=' + encodeURIComponent(url), function(data) {
+    setTimeout(updateGoSquared, 5000);
+    $('#online-now').addClass('visible').find('.visitors').text(data.visitors);
+  });
+};
+
+updateGoSquared();
