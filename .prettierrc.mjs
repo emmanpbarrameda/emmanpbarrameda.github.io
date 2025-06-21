6cf06c884f0207d5b62dea007f36de0ede546c63
EMMAN <emmanuelbarrameda1@gmail.com>
2025-06-22 01:30:21 +0800

    62225 130AM Execped info

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,4 +1,13 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: ["prettier-plugin-astro"],
+  printWidth: 9999,
+  htmlWhitespaceSensitivity: "ignore",
+  proseWrap: "never"
 };
+
+// ORIG CODE
+// /** @type {import("prettier").Config} */
+// export default {
+//   plugins: ["prettier-plugin-astro"],
+// };
