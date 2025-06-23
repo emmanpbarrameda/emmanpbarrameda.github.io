3e1923552f7372744705079c57dd18e4350e4ef4
EMMAN <emmanuelbarrameda1@gmail.com>
2025-06-23 16:17:04 +0800

    062625 416PM auto info added

---
+++ src/data/infos.js
@@ -0,0 +1,21 @@
+export const infos = {
+  host: "https://emmanpbarrameda.github.io",
+
+  fullname: "Emmanuel P. Barrameda",
+
+  username: "emmanpbarrameda",
+
+  email: "emmanpbarrameda@gmail.com",
+
+  github: "https://github.com/emmanpbarrameda",
+  githubtext: "emmanpbarrameda",
+
+  linkedin: "https://www.linkedin.com/in/emmanpbarrameda/",
+  linkedintext: "Emman P. Barrameda",
+
+  calmeeting: "https://cal.com/emmanpbarrameda/",
+  calmeetingtext: "cal.com/emmanpbarrameda",
+
+  resume: "https://github.com/emmanpbarrameda/EMMANPBARRAMEDA_RESUME/blob/main/RESUME/EMMANPBARRAMEDA_RESUME.pdf",
+  moreprojects: "https://github.com/emmanpbarrameda?tab=repositories&q=&type=public&language=&sort=",
+};
--- src/data/socials.js
+++
@@ -1,7 +0,0 @@
-// src/data/socials.js
-
-export const socials = {
-  github: "https://github.com/emmanpbarrameda",
-  linkedin: "https://www.linkedin.com/in/emmanpbarrameda/",
-  resume: "https://github.com/emmanpbarrameda/EMMANPBARRAMEDA_RESUME/blob/main/RESUME/EMMANPBARRAMEDA_RESUME.pdf",
-};
