5e2fa0018775ec4ab39a651502f49c615ddfc5c2
EMMAN <emmanuelbarrameda1@gmail.com>
2025-09-28 23:20:21 +0800

    092825 1120PM Page Improvements

---
+++ src/components/credentials/credentials_data.js
@@ -0,0 +1,49 @@
+export const experience = [
+  { title: "Software Developer", org: "Casterly Philippines", date: "Aug 2025 – Present", },
+  { title: "Backend Developer Intern", org: "Bapplware Technologies Inc.", date: "Feb 2025 – Apr 2025", },
+  { title: "Freelancer", date: "May 2020 – Present" },
+];
+
+export const leadership = [
+  { title: "Vice President for Corporate Communications", org: "URSB Alumni Association", date: "Aug 2025 – Present", notes: "Organization", href: "https://www.facebook.com/share/1AgMYQn7Cv/" },
+  { title: "Tech Specialist (VP Programs Office)", org: "DEVCON Manila", date: "May 2025 – Present", notes: "Community Organization", href: "https://www.facebook.com/share/17HsuCAcyN/" },
+  { title: "President", org: "URSB BSIT Students' League", date: "Aug 2024 – Jun 2025", notes: "Organization", href: "https://www.facebook.com/share/1AamWmzH9u/" },
+  { title: "Public Information Officer", org: "URSB Graduating Class of 2025", date: "Feb 2025 – Jun 2025", notes: "Organization", href: "https://www.facebook.com/ursb.graduatingclass2025" },
+  { title: "Class President, IT 4-2", date: "AY 2024–2025, 2nd Sem" },
+  { title: "Class Vice President, IT 4-2", date: "AY 2024–2025, 1st Sem" },
+  { title: "Member of Creatives Committee", org: "URS Binangonan Electoral Tribunal", date: "Aug 2025 – Oct 2025", notes: "Organization", href: "https://www.facebook.com/share/1ED5by6zRK/" },
+  { title: "Vice President", org: "URSB BSIT Students' League", date: "Aug 2023 – Jun 2024", notes: "Organization", href: "https://www.facebook.com/share/1FDdAuDvEd/" },
+  { title: "Class Vice President, IT 3-2", date: "AY 2023–2024" },
+  { title: "Deputy", org: "URSB BSIT Students' League", date: "Aug 2022 – May 2023", notes: "Organization", },
+  { title: "Class President, IT 2-2", date: "AY 2022–2023, 2nd Sem" },
+  { title: "Class Auditor, IT 2-2", date: "AY 2022–2023, 1st Sem" },
+  { title: "Member, VMIS ICT CLUB - URS Binangonan", date: "Jun 2018 – Apr 2020", notes: "School Club", },
+  { title: "Class Vice President, 12-San Antonio", date: "AY 2019–2020" },
+  { title: "Class President, 11-Tatala", date: "AY 2018–2019, 2nd Sem" },
+  { title: "Youth President, Mabuhay UMYF", date: "2017–2019" },
+];
+
+export const achievements = [
+  { title: "Best Software Developmental Study", org: "URS Binangonan", date: "2025", notes: "Group Leader & Lead Develeoper", href: "https://www.facebook.com/share/p/16wnouynXf/" },
+  { title: "Best ICT Student", org: "Vicente Madrigal Integrated School", date: "2020", notes: "Group Leader & Lead Develeoper" },
+  { title: "Best in Research", org: "VMIS", date: "2020", notes: "Team Leader & Lead Dev" },
+  { title: "With Honors", org: "VMIS", date: "2020" },
+];
+
+export const speakerships = [
+    { title: "Capstone Success: Tips & Techniques", org: "URSB BSIT Students' League", date: "Sept. 2024", notes: "Resource Speaker", href: "https://www.facebook.com/share/p/14KYWomoi71/" },
+];
+
+export const engagements = [
+  { title: "Google I/O 2025 Manila", date: "Participant" },
+  { title: "Design in Motion w/ LottieFiles & Figma 2025", date: "Participant" },
+  { title: "UMAK 13th IT Skills Olympics", date: "Participant" },
+  { title: "GitHub Field Day 2024", date: "Participant" },
+];
+
+export const communities = [
+    { title: "Student Developer Philippines (StuDevPH)" },
+    { title: "DEVCON Manila" },
+    { title: "Filipino Web Development Peers (FWDP)" },
+    { title: "AngularPH" }, 
+];
